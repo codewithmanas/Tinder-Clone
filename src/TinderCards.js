e6f3d8dc8321c0345f47a01f3203bfe56fe979d2
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-04-14 14:45:32 +0530

    Hooking of our Front-End and Back-End

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,49 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./TinderCards.css";
 import TinderCard from 'react-tinder-card';
+import axios from './axios';
 
 function TinderCards() {
 
-  const [ people, setPeople ] = useState([
-    {
-      name: "Elon Musk",
-      url: "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg"
-    },
-    {
-      name: "Jeff Bezos",
-      url: "https://m.media-amazon.com/images/M/MV5BYTNlOGZhYzgtMmE3OC00Y2NiLWFhNWQtNzg5MjRhNTJhZGVmXkEyXkFqcGdeQXVyNzg5MzIyOA@@._V1_.jpg"
+  const [ people, setPeople ] = useState([]);
+
+  // const fetchData = async () => {
+  //   const req = await axios.get("/tinder/cards");
+  //   setPeople(req.data);
+  // }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const req = await axios.get("/tinder/cards");
+      setPeople(req.data);
     }
-  ]);
-  // setPeople(['This People']);
+    fetchData();
+  }, []);
 
   const swiped = (direction, nameToDelete) => {
-        console.log("removing: " + nameToDelete);
-        // setLastDirection(direction);
+    console.log("removing: " + nameToDelete);
   }
+
   const outOfFrame = (PersonName) => {
-     console.log(PersonName + "left the screen");
+    console.log(PersonName + "left the screen");
   }
 
   return (
     <div className="tinder__cards">
         <div className="cards__container container">
-            {people.map((person) => {
-              return (
-              <>
-              <TinderCard className='swipe'
+            {people.map((person) => (
+              <TinderCard
+                className='swipe'
                 key={person.name}
                 preventSwipe={["up", "down"]}
                 onSwipe={(dir) => swiped(dir, person.name)}
-                onCardLeftScreen = {() => outOfFrame(person.name)}
+                onCardLeftScreen={() => outOfFrame(person.name)}
               >
-              <div style={{ backgroundImage: `url(${person.url})` }}
-                  className="card">
-                <h3>{person.name}</h3>
-              </div>
+                <div
+                  style={{ backgroundImage: `url(${person.imgUrl})` }}
+                  className="card"
+                >
+                  <h3>{person.name}</h3>
+                </div>
               </TinderCard>
-              </>
-              )
-            })}
+            ))}
         </div>
     </div>
   );
